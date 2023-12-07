401cc3a8325b01a9e8a3413c09b6c80e56d6ee00
Lucky_Armin <armin.penz@student.uibk.ac.at>
2023-12-07 07:51:56 +0100

    Update

--- 1/day1_part2.c
+++ 1/day1_part2.c
@@ -32,8 +32,9 @@ int main(int argc, char *argv[])
 	int  number_of_numbers = 0;
 	int  summand = 0;
 	int  sum = 0;
-	char written_out_number[10];
+	char *written_out_number;
 	char three_letter_numbers[10];
+	written_out_number = (char *)calloc(5, sizeof(char));
 
 	input_file = fopen(input_filename, "r");
 
@@ -49,11 +50,10 @@ while (fgets(line, sizeof(line), input_file) != NULL)
         if(c>='0' && c<='9')
 		{
 			numeric_digit = c - '0';
-			numbers_of_this_line[string_position] = numeric_digit;
-			string_position++;
+			numbers_of_this_line[string_position++] = numeric_digit;
 			number_of_numbers++;
 		} else {
-			while(isalpha(string[j])){
+			while(isalpha(string[j]) != 0){
 				written_out_number[k++] = string[j++];
 			}
 
@@ -63,21 +63,26 @@ while (fgets(line, sizeof(line), input_file) != NULL)
 		three_letter_numbers[1] = written_out_number[1];
 		three_letter_numbers[2] = written_out_number[2];
 
+		printf("%s\n" , written_out_number);
+
 		if(strcmp(three_letter_numbers, "one") == 0){
 			numbers_of_this_line[string_position++] = 1;
+			number_of_numbers++;
 		}
 		if(strcmp(three_letter_numbers, "two") == 0){
 			numbers_of_this_line[string_position++] = 2;
+			number_of_numbers++;
+
 		}		
 		if(strcmp(three_letter_numbers, "six") == 0){
 			numbers_of_this_line[string_position++] = 6;
+			number_of_numbers++;
+
 		}
 
       }
 
-
-
-
+	written_out_number = realloc(written_out_number, 5 * sizeof(char));
 
 	if(number_of_numbers == 1){
 		summand = numbers_of_this_line[0] * 10 + numbers_of_this_line[0];
